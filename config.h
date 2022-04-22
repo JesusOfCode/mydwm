68fe9dbd5bf08cbe33f180f54d5ff0260800a493
heikki <sulinheikki@gmail.com>
2022-04-22 15:45:09 +0300

    Tweaks

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const double defaultopacity  = 1.0;
-static const char *fonts[]          = { "Anonymice Nerd Font Mono:size=14"};
+static const char *fonts[]          = { "TerminessTTF Nerd Font:size=14"};
 static const char dmenufont[]       = "monospace:size=10";
 //bg color
 static const char col_gray1[]       = "#000000";
@@ -76,24 +76,24 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,             			XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,				XK_s,	   spawn,	  	   SHCMD("transset -a --dec .1") },
-	{ MODKEY|ShiftMask,				XK_d,	   spawn,	  	   SHCMD("transset -a --inc .1") },
-	{ MODKEY|ShiftMask,				XK_f,	   spawn,	 	   SHCMD("transset -a 1.0") },
+	//{ MODKEY|ShiftMask,				XK_s,	   spawn,	  	   SHCMD("transset -a --dec .1") },
+	//{ MODKEY|ShiftMask,				XK_d,	   spawn,	  	   SHCMD("transset -a --inc .1") },
+	//{ MODKEY|ShiftMask,				XK_f,	   spawn,	 	   SHCMD("transset -a 1.0") },
 	{ MODKEY,             			XK_w, 	   spawn,          SHCMD("librewolf") },
 	{ MODKEY|ShiftMask,        		XK_w, 	   spawn,          SHCMD(TERMINAL " -e lfub && wal -R && clear") },
 	{ MODKEY,             			XK_Insert, spawn,  		   SHCMD("flameshot gui") },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	//{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	//{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_Left,   setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_Right,  setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_s, 	   zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,		                XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
