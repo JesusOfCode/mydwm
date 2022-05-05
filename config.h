8c1a67c824b5108ea34f9015cedcee7f207a7245
heikki <sulinheikki@gmail.com>
2022-05-05 18:09:59 +0300

    Tweaks

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
 #define TERMCLASS "Alacritty"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 29;        /* gaps between windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 8;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
